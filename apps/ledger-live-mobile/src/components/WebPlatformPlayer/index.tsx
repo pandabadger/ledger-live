58f819bef9bd901330598dd0f719a3919e40b9d2
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2022-08-17 14:30:35 +0200

    fix: Update preparedMessageType in mobile
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- apps/ledger-live-mobile/src/components/WebPlatformPlayer/index.tsx
+++ apps/ledger-live-mobile/src/components/WebPlatformPlayer/index.tsx
@@ -47,7 +47,7 @@ import {
   serializePlatformSignedTransaction,
   deserializePlatformSignedTransaction,
 } from "@ledgerhq/live-common/platform/serializers";
-import { prepareMessageToSign } from "@ledgerhq/live-common/lib/hw/signMessage";
+import { prepareMessageToSign } from "@ledgerhq/live-common/hw/signMessage/index";
 import {
   useListPlatformAccounts,
   useListPlatformCurrencies,
