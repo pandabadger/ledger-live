4a197d785ead0af019c60a9669f200cb58a42671
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2022-08-23 14:42:47 +0200

    Fixes WETH module to only remap the WETH token from Ethereum

--- libs/ledger-live-common/src/countervalues/modules/weth.ts
+++ libs/ledger-live-common/src/countervalues/modules/weth.ts
@@ -1,18 +1,13 @@
-import type { Module } from "./types";
-import { getCryptoCurrencyById } from "../../currencies";
+import type { Module, Pair } from "./types";
+import { getCryptoCurrencyById, getTokenById } from "../../currencies";
+
 const ETH = getCryptoCurrencyById("ethereum");
 
-const remap = (pair) => {
-  if (pair.from.ticker === "WETH")
-    return {
-      from: ETH,
-      to: pair.to,
-    };
-  if (pair.to.ticker === "WETH")
-    return {
-      from: pair.from,
-      to: ETH,
-    };
+const wethId = "ethereum/erc20/weth";
+
+const remap = (pair: Pair): Pair => {
+  if (pair.from.id === wethId) return { from: ETH, to: pair.to };
+  if (pair.to.id === wethId) return { from: pair.from, to: ETH };
   return pair;
 };
 
