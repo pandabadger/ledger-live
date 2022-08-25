a03ac751a47dc2c839b2e1edfba6d1f39550b9ef
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-08-25 12:21:03 +0200

    Remove fake url entrypoint

--- apps/ledger-live-mobile/src/components/CustomImage/CustomImageBottomModal.tsx
+++ apps/ledger-live-mobile/src/components/CustomImage/CustomImageBottomModal.tsx
@@ -12,18 +12,6 @@ type Props = {
   onClose: BottomModalProps["onClose"];
 };
 
-const testUrls = {
-  boredApe: "https://img.phonandroid.com/2022/04/bored-ape-yacht-club.jpg",
-  chessboard:
-    "https://media.istockphoto.com/vectors/checkered-pattern-black-and-white-vector-id806894546?k=20&m=806894546&s=612x612&w=0&h=ci54bNwp8kTWiyNqtUKgqMkOau_Hl875T66oiEMbH64=",
-  bigImage:
-    "https://effigis.com/wp-content/uploads/2015/02/Airbus_Pleiades_50cm_8bit_RGB_Yogyakarta.jpg",
-  hugeImage:
-    "https://upload.wikimedia.org/wikipedia/commons/9/9d/Pieter_Bruegel_the_Elder_-_The_Fall_of_the_Rebel_Angels_-_Google_Art_Project.jpg",
-  veryThinHugeImage:
-    "https://cdn.theatlantic.com/assets/media/img/posts/NbYbNrs.png",
-};
-
 const CustomImageBottomModal: React.FC<Props> = props => {
   const [isLoading, setIsLoading] = useState(false);
   const { onClose } = props;
@@ -50,16 +38,6 @@ const CustomImageBottomModal: React.FC<Props> = props => {
     onClose && onClose();
   }, [navigation, onClose, setIsLoading]);
 
-  const handleFromUrl = useCallback(() => {
-    navigation.navigate(NavigatorName.CustomImage, {
-      screen: ScreenName.CustomImageStep1Crop,
-      params: {
-        imageUrl: testUrls.boredApe,
-      },
-    });
-    onClose && onClose();
-  }, [navigation, onClose]);
-
   return (
     <BottomModal isOpened={props.isOpened} onClose={props.onClose}>
       <Text variant="h4" fontWeight="semiBold" pb={5}>
@@ -74,12 +52,6 @@ const CustomImageBottomModal: React.FC<Props> = props => {
           <ModalChoice
             onPress={handleUploadFromPhone}
             title={t("customImage.drawer.options.uploadFromPhone")}
-            iconName={"ArrowFromBottom"}
-            event=""
-          />
-          <ModalChoice
-            onPress={handleFromUrl}
-            title={"(debug) from fixed URL"}
             iconName={"ArrowFromBottom"}
             event=""
           />
